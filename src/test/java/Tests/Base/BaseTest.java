a2c66aa917eaf7b7c17b48f18a708279077ec577
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-08-30 15:05:30 +0300

    modify BaseTest

--- src/test/java/Tests/Base/BaseTest.java
+++ src/test/java/Tests/Base/BaseTest.java
@@ -1,3 +1,11 @@
+package Tests.Base;
+
+import Pages.CartPage;
+import Pages.Checkout.CompletePage;
+import Pages.Checkout.OverviewPage;
+import Pages.Checkout.YourInformationPage;
+import Pages.LoginPage;
+import Pages.ProductPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,7 +17,13 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    WebDriver browser;
+    public WebDriver browser;
+    public LoginPage loginPage;
+    public ProductPage productPage;
+    public CartPage cartPage;
+    public YourInformationPage yourInformationPage;
+    public OverviewPage overviewPage;
+    public CompletePage completePage;
 
     @BeforeMethod
     public void setup() {
@@ -17,6 +31,12 @@ public class BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         browser = new ChromeDriver(options);
+        loginPage = new LoginPage(browser);
+        productPage = new ProductPage(browser);
+        cartPage = new CartPage(browser);
+        yourInformationPage = new YourInformationPage(browser);
+        overviewPage = new OverviewPage(browser);
+        completePage = new CompletePage(browser);
         browser.manage().window().maximize();
         browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
@@ -26,15 +46,10 @@ public class BaseTest {
         browser.quit();
     }
 
-    public void login(String username, String password) {
-        browser.findElement(By.id("user-name")).sendKeys(username);
+    public void login(String USERNAME, String password) {
+        browser.findElement(By.id("user-name")).sendKeys(USERNAME);
         browser.findElement(By.id("password")).sendKeys(password);
         browser.findElement(By.id("login-button")).click();
     }
-
-    public static String priceOfGood(Map<String, String> map, String goodName) {
-        return map.get(goodName);
-    }
-
 }
 

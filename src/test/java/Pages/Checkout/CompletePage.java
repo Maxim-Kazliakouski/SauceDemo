76c56559e11a3e6b0433f62b5681c115fd7b8146
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-08-30 15:13:55 +0300

    add Complete page

---
+++ src/test/java/Pages/Checkout/CompletePage.java
@@ -0,0 +1,32 @@
+package Pages.Checkout;
+
+import Pages.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class CompletePage extends BasePage {
+    private final By TITLE_COMPLETE = By.xpath("//span[contains(text(),'Checkout: Complete!')]");
+    public final By COMPLETE_HEADER = By.xpath("//h2[contains(text(),'THANK YOU FOR YOUR ORDER')]");
+    private final By BACK_HOME_BUTTON = By.id("back-to-products");
+
+    public CompletePage(WebDriver browser) {
+        super(browser);
+    }
+
+    public void open() {
+        String COMPLETE_PAGE_URL = BASE_URL + "checkout-complete.html";
+        browser.get(COMPLETE_PAGE_URL);
+    }
+
+    public boolean isOpened() {
+        return waitForVisibility(TITLE_COMPLETE);
+    }
+
+    public boolean isHeaderTextVisible() {
+        return waitForVisibility(COMPLETE_HEADER);
+    }
+
+    public void clickBackToHome() {
+        browser.findElement(BACK_HOME_BUTTON).click();
+    }
+}

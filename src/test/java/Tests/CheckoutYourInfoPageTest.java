07588879dc2a119688fb24c97a125c919b5314c9
max_kazliakouski <maxim.kazliakouski@gmail.com>
2022-08-31 21:55:46 +0300

    add parametrize test and description

--- src/test/java/Tests/CheckoutYourInfoPageTest.java
+++ src/test/java/Tests/CheckoutYourInfoPageTest.java
@@ -2,14 +2,14 @@ package Tests;
 
 import Pages.BasePage;
 import Tests.Base.BaseTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class CheckoutYourInfoPageTest extends BaseTest {
 
-    @Test
+    @Test(description = "User should be redirect to the Overview page from Your Info page after clicking on 'Continue' button")
     public void redirectionToOverviewPage() {
         loginPage.open();
         assertTrue(loginPage.isOpened(), "Login page wasn't opened");
@@ -20,47 +20,26 @@ public class CheckoutYourInfoPageTest extends BaseTest {
         yourInformationPage.clickContinue();
         assertTrue(overviewPage.isOpened(), "Checkout: Overview page wasn't opened!");
     }
-
-    @Test
-    public void errorMessageAfterMissingRequiredFieldFirstName() {
-        loginPage.open();
-        assertTrue(loginPage.isOpened(), "Login page wasn't opened");
-        loginPage.login(BasePage.USERNAME, BasePage.PASSWORD);
-        yourInformationPage.open();
-        assertTrue(yourInformationPage.isOpened(), "User hasn't been redirected to the checkout confirmation page");
-        yourInformationPage.fillCheckoutFields("", "Ivanov", "200100");
-        yourInformationPage.clickContinue();
-        assertTrue(yourInformationPage.errorIsAppeared(), "There is no any error message after " +
-                "missing required field 'First Name'");
-        assertTrue(yourInformationPage.correctErrorMessageText("First Name"), "Error message text has been changed");
+    @DataProvider
+    public Object[][] errorMessage(){
+        return new Object[][]{
+                {"", "Ivanov", "200100", "Error: First Name is required"},
+                {"Ivan", "", "200100", "Error: Last Name is required"},
+                {"Ivan", "Ivanov", "", "Error: Postal Code is required"}
+        };
     }
-
-    @Test
-    public void errorMessageAfterMissingRequiredFieldLastName() {
+    @Test(dataProvider = "errorMessage", description = "Error message should be appear, if user doesn't fill required field")
+    public void errorMessageAfterMissingRequiredField(String firstName, String lastName, String zip, String errorMessage) {
         loginPage.open();
         assertTrue(loginPage.isOpened(), "Login page wasn't opened");
         loginPage.login(BasePage.USERNAME, BasePage.PASSWORD);
         yourInformationPage.open();
         assertTrue(yourInformationPage.isOpened(), "User hasn't been redirected to the checkout confirmation page");
-        yourInformationPage.fillCheckoutFields("Ivan", "", "200100");
+        yourInformationPage.fillCheckoutFields(firstName, lastName, zip);
         yourInformationPage.clickContinue();
         assertTrue(yourInformationPage.errorIsAppeared(), "There is no any error message after " +
-                "missing required field 'Last Name'");
-        assertTrue(yourInformationPage.correctErrorMessageText("Last Name"), "Error message text has been changed!");
-    }
-
-    @Test
-    public void errorMessageAfterMissingRequiredFieldZip() {
-        loginPage.open();
-        assertTrue(loginPage.isOpened(), "Login page wasn't opened");
-        loginPage.login(BasePage.USERNAME, BasePage.PASSWORD);
-        yourInformationPage.open();
-        assertTrue(yourInformationPage.isOpened(), "User hasn't been redirected to the checkout confirmation page");
-        yourInformationPage.fillCheckoutFields("Ivan", "Ivanov", "");
-        yourInformationPage.clickContinue();
-        assertTrue(yourInformationPage.errorIsAppeared(), "There is no any error message after " +
-                "missing required field 'Postal Code'");
-        assertTrue(yourInformationPage.correctErrorMessageText("Postal Code"), "Error message text has been changed!");
+                "missing required field!");
+        assertEquals(yourInformationPage.getErrorMessage(), errorMessage, "Error message text has been changed");
     }
 
     @Test
